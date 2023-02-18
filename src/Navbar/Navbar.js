d76bc23078be7e7f257fb3364f1f950bd226fad9
bnc1338 <bencebarnabasszocs@gmail.com>
2023-02-18 17:39:36 +0200

    little fixes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,32 +3,41 @@ import './Navbar.css';
 import Image from '../Icons/PinPiros.png'
 import logoSeven from '../Icons/logo-seven.png';
 import "../MainCSS.css";
+import logoSevenWhite from '../Icons/logo-seven-white.png';
 
 const Navbar = ()=>{
     const [isOpen, setIsOpen] = useState(false);
     const [isClosed, setIsClosed] = useState(true);
+    const [isScrolled, setIsScrolled] = useState(false);
       window.addEventListener('scroll', ()=> {
         const navbar = document.getElementById('navArea');
-        const scrolly=window.scrollY;
+        const scrolly = window.scrollY;
+        const navlogo = document.getElementById('nav-logo-img');
 
-        if(scrolly>0){
-            navbar.style.backgroundColor="rgba(255, 255, 255, 0.5)";
-          }
-          else if(scrolly===0){
+        if(scrolly<=200){
             navbar.style.backgroundColor="rgba(255, 255, 255, 1)";
-          }
+            navlogo.src=logoSeven;
+            setIsScrolled(false);
+        }
+        else{
+            navbar.style.backgroundColor="rgba(0, 0, 0, 0.9)";
+            if(window.innerWidth > 700){navlogo.src=logoSevenWhite;
+            }
+            setIsScrolled(true);
+        }
+
       });
 
     return (
         <div className="Navbar">
             <div id='navArea' className={`nav-container ${isOpen && "open"} ${isClosed && "closed"}`}>
                 <div className='nav-logo unselectable'>
-                    <img src={logoSeven} alt="" width={"100px"} height={"auto"}/>    
+                    <img id='nav-logo-img' src={logoSeven} alt="" width={"100px"} height={"auto"} />    
                 </div>
                 <div className={`nav-items ${isOpen && "open"} unselectable`}>
-                    <a href='/about' >About Me</a>
-                    <a href='/insta'>Programs</a>
-                    <a href='https://bnc1338.github.io/attika7ifbbpro/'>Contact me!</a>
+                    <a href='/about' className={`${isScrolled ? "nav-item-change-color":""}`}>About Me</a>
+                    <a href='/insta' className={`${isScrolled ? "nav-item-change-color":""}`}>Programs</a>
+                    <a href='https://bnc1338.github.io/attika7ifbbpro/' className={`${isScrolled ? "nav-item-change-color":""}`}>Contact me!</a>
                 </div>
                 <a href='https://bnc1338.github.io/attika7ifbbpro/'>
                     <img src={Image} alt="" className='navbar-img'/>
